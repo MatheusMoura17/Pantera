aad47def695b019bf05061ee4056349c0e192fca
Matheus Moura <matheus.moura13@gmail.com>
2019-05-14 22:52:51 -0300

    Adiciona script de treino

--- panther-audio-detector/services/constants.ts
+++ panther-audio-detector/services/constants.ts
@@ -4,5 +4,10 @@ export const labels: string[] = ['false', 'true'];
 export const mfccSize: number = 13;
 export const xSize: number = 50;
 export const xStep: number = 25;
+
 export const testSplit: number = 0.2;
 export const learningRate: number = 0.001;
+export const batchSize: number = 100;
+export const epochs: number = 10;
+
+export const modelName: string = 'panther-audio-detector';
---
+++ panther-audio-detector/src/train.ts
@@ -0,0 +1,32 @@
+import * as tf from '@tensorflow/tfjs-node';
+import fs from 'fs-extra';
+
+import { IData } from '../services/types';
+import { batchSize, epochs, modelName, dataBasePath } from '../services/constants';
+import { dataToTensors } from './data';
+import { createModel } from './model';
+
+const train = async () => {
+    console.log('Reading Data');
+    const data: IData = fs.readJSONSync(`${dataBasePath}/data.json`);
+
+    console.log('Converting data to tensors');
+    const { xTrain, yTrain, xTest, yTest } = dataToTensors(data);
+
+    console.log('Creating model');
+    const model = createModel();
+    model.summary();
+
+    console.log('Training model');
+    const result = await model.fit(xTrain, yTrain, {
+        batchSize,
+        epochs,
+        validationData: [xTest, yTest],
+    });
+
+    console.log(result.history);
+
+    model.save(`file://${dataBasePath}/model/${modelName}`);
+};
+
+train();
