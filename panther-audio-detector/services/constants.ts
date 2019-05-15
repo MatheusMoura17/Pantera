41460858bb762b38b14d55d31ba4ef8b8b9bcc25
Matheus Moura <matheus.moura13@gmail.com>
2019-05-14 21:47:52 -0300

    Cria constantes necessarias para o pre-processador

--- panther-audio-detector/services/constants.ts
+++ panther-audio-detector/services/constants.ts
@@ -1,1 +1,7 @@
-export default 
+export const dataBasePath: string = './data';
+export const bufferSize: number = 512;
+export const labels: string[] = ['false', 'true'];
+export const mfccSize = 13;
+export const xSize = 50;
+export const xStep = 25;
+export const testSplit = 0.2;
